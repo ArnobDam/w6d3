25141887fd2af658d426d9eedbc58cdd75cce1ff
Evan Ryan <evan.hundred@gmail.com>
2022-10-19 16:39:55 -0400

    edit create method for users controller

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        # render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        # user = User.new(params)
+        # user.save!
+        # render json: user
+
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
